c0708f3a1f753048cdcaca5aa555831958b1a199
KingdomKlyuch <kneedy93@gmail.com>
2022-12-30 18:01:26 +0200

    Seperate book and library function add remove fnc

--- script.js
+++ script.js
@@ -1,15 +1,64 @@
+const libraryElement = document.getElementById("library");
+const bookTemplateElement = document.getElementById("book-template");
+const addBookFormElement = document.getElementById("add-book-form");
+const library = [];
+
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author; 
-    this.pages = pages;
-    this.read = read;
-  
-    this.info = function() {
-      // eslint-disable-next-line prefer-const
-      let readStatus = this.read ? "has been read" : "not read yet";
-      return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function() {
+    let readStatus = this.read ? "has been read" : "not read yet";
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
   }
-  
-  let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-  console.log(theHobbit.info());
+}
+
+function addBookToLibrary(book) {
+  if (library.length >= 8) {
+    console.log("Error: Library is full. Cannot add more books.");
+  } else {
+    library.push(book);
+    const bookElement = document.importNode(bookTemplateElement.content, true);
+    bookElement.querySelector(".book-title").textContent = book.title;
+    bookElement.querySelector(".book-author").textContent = book.author;
+    bookElement.querySelector(".book-read-status").checked = book.read;
+    bookElement.querySelector(".book-remove-button").addEventListener("click", () => {
+      removeBookFromLibrary(book);
+    });
+
+function removeBookFromLibrary(book) {
+  let index = library.indexOf(book);
+  if (index > -1) {
+    library.splice(index, 1);
+    libraryElement.removeChild(book.element);
+  } else {
+    console.log("Error: Book not found in library.");
+  }
+}
+
+function changeReadStatus(book, isRead) {
+  let index = library.indexOf(book);
+  if (index > -1) {
+    library[index].isRead = isRead;
+  } else {
+    console.log("Error: Book not found in library.");
+  }
+}
+
+addBookFormElement.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const title = event.target.elements["book-title-input"].value;
+  const author = event.target.elements["book-author-input"].value;
+
+  const book = {
+    title: title,
+    author: author,
+    isRead: false
+  };
+
+  addBookToLibrary(book);
+});
+
