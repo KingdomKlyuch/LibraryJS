84497ea5789e7ebeba55633a691742c3c34f0ac6
KingdomKlyuch <kneedy93@gmail.com>
2022-12-30 20:41:09 +0200

    Remove contradictory function addFormElement

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 const libraryElement = document.getElementById("library");
 const bookTemplateElement = document.getElementById("book-template");
-const addBookFormElement = document.getElementById("add-book-form");
 const library = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, read) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
   this.read = read;
 
   this.info = function() {
     let readStatus = this.read ? "has been read" : "not read yet";
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+    return `${this.title} by ${this.author}, ${readStatus}`;
   }
 }
 
 function addBookToLibrary(book) {
-  if (library.length >= 8) {
-    console.log("Error: Library is full. Cannot add more books.");
-  } else {
-    library.push(book);
-    const bookElement = document.importNode(bookTemplateElement.content, true);
-    bookElement.querySelector(".book-title").textContent = book.title;
-    bookElement.querySelector(".book-author").textContent = book.author;
-    bookElement.querySelector(".book-read-status").checked = book.read;
-    bookElement.querySelector(".book-remove-button").addEventListener("click", () => {
-      removeBookFromLibrary(book);
-    });
+  library.push(book);
+  const bookElement = document.importNode(bookTemplateElement.content, true);
+  bookElement.querySelector(".book-title").textContent = book.title;
+  bookElement.querySelector(".book-author").textContent = book.author;
+  bookElement.querySelector(".book-read-status").checked = book.read;
+  bookElement.querySelector(".book-remove-button").addEventListener("click", () => {
+    resetBookElement(book);
+  });
 
-function removeBookFromLibrary(book) {
+  // Add a change event listener to the book's read status checkbox
+  bookElement.querySelector(".book-read-status").addEventListener("change", () => {
+    changeReadStatus(book, event.target.checked);
+  });
+
+  libraryElement.appendChild(bookElement);
+}
+
+function resetBookElement(book) {
   let index = library.indexOf(book);
   if (index > -1) {
-    library.splice(index, 1);
-    libraryElement.removeChild(book.element);
+    library[index] = new Book("Title", "Author", false);
+    const bookElement = libraryElement.querySelector(`[data-book-id="${book.id}"]`);
+    bookElement.querySelector(".book-title").textContent = "Title";
+    bookElement.querySelector(".book-author").textContent = "Author";
+    bookElement.querySelector(".book-read-status").checked = false;
   } else {
     console.log("Error: Book not found in library.");
   }
@@ -41,24 +47,14 @@ function removeBookFromLibrary(book) {
 function changeReadStatus(book, isRead) {
   let index = library.indexOf(book);
   if (index > -1) {
-    library[index].isRead = isRead;
+    library[index].read = isRead;
   } else {
     console.log("Error: Book not found in library.");
   }
 }
 
-addBookFormElement.addEventListener("submit", event => {
-  event.preventDefault();
-
-  const title = event.target.elements["book-title-input"].value;
-  const author = event.target.elements["book-author-input"].value;
-
-  const book = {
-    title: title,
-    author: author,
-    isRead: false
-  };
-
-  addBookToLibrary(book);
-});
-
+// Add default book elements to the library
+for (let i = 0; i < 8; i++) {
+  const defaultBook = new Book("Title", "Author", false);
+  addBookToLibrary(defaultBook);
+}
