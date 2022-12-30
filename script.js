adfffbad08cd9b9ed5dd2d85971dffac9b2d26a7
KingdomKlyuch <kneedy93@gmail.com>
2022-12-30 23:38:46 +0200

    Add working page function

--- script.js
+++ script.js
@@ -3,53 +3,56 @@ window.onload = function() {
     const bookTemplateElement = document.getElementById("book-template");
     const library = [];
   
-    function Book(title, author, read) {
-      this.title = title;
-      this.author = author;
-      this.read = read;
-    }
-  
-    function addBookToLibrary(book) {
-        library.push(book);
-        const bookElement = document.importNode(bookTemplateElement.content, true);
-        bookElement.querySelector(".book-title").textContent = book.title;
-        bookElement.querySelector(".book-author").textContent = book.author;
-        bookElement.querySelector(".book-read-status").checked = book.read;
-        bookElement.querySelector(".book-remove-button").addEventListener("click", () => {
-          resetBookElement(book);
-        });
-      
-        // Add a click event listener to the book's read status checkmark
-        bookElement.querySelector(".book-read-status").addEventListener("click", () => {
-          toggleReadStatus(book);
-        });
-      
-        libraryElement.appendChild(bookElement);
+    function Book(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
       }
   
-    function changeReadStatus(book, isRead) {
-      let index = library.indexOf(book);
-      if (index > -1) {
-        library[index].read = isRead;
-      } else {
-        console.log("Error: Book not found in library.");
+function addBookToLibrary(book) {
+  library.push(book);
+  const bookElement = document.importNode(bookTemplateElement.content, true);
+  bookElement.querySelector(".book-title").textContent = book.title;
+  bookElement.querySelector(".book-author").textContent = book.author;
+  bookElement.querySelector(".book-pages").textContent = `${book.pages} pages`;
+  bookElement.querySelector('[name="book-read-status"]').checked = book.read;
+  bookElement.querySelector(".book-remove-button").addEventListener("click", () => {
+    resetBookElement(book);
+  });
+
+  // Add a click event listener to the book's read status checkmark
+  bookElement.querySelector('[name="book-read-status"]').addEventListener("click", () => {
+    toggleReadStatus(book);
+  });
+
+  libraryElement.appendChild(bookElement);
+}
+
+
+
+    function toggleReadStatus(book) {
+        book.read = !book.read;
       }
-    }
+      
   
-    const addBookForm = document.getElementById("add-book-form");
-    addBookForm.addEventListener("submit", event => {
-      event.preventDefault();
-  
-      const titleInput = document.getElementById("book-title-input");
-      const authorInput = document.getElementById("book-author-input");
-      const title = titleInput.value;
-      const author = authorInput.value;
-  
-      const book = new Book(title, author, false);
-  
-      addBookToLibrary(book);
-    });
-    
+      const addBookForm = document.getElementById("add-book-form");
+      addBookForm.addEventListener("submit", event => {
+        event.preventDefault();
+      
+        const titleInput = document.getElementById("book-title-input");
+        const authorInput = document.getElementById("book-author-input");
+        const pagesInput = document.getElementById("book-pages-input");
+        const title = titleInput.value;
+        const author = authorInput.value;
+        const pages = pagesInput.value;
+      
+        const book = new Book(title, author, pages, false);
+      
+        addBookToLibrary(book);
+      });
+      
+
     function resetBookElement(book) {
         const bookElements = Array.from(libraryElement.querySelectorAll(".book"));
         const bookElement = bookElements.find(element => element.querySelector(".book-title").textContent === book.title);
